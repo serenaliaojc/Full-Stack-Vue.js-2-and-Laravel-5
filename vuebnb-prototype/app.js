50645ce6b917882e8c88eaae77b54a9fab8eafcb
serenaliaojc <36046972+serenaliaojc@users.noreply.github.com>
2019-06-27 12:40:45 +0800

    prototype: data binding

--- vuebnb-prototype/app.js
+++ vuebnb-prototype/app.js
@@ -1,6 +1,8 @@
 var app = new Vue({
     el: '#app',
     data: {
-        title: 'My Appointment'
+        title: 'My Appointment',
+        address: '12 My Street, My City, My Country',
+        about: 'This is a description of my apartment.'
     }
 });

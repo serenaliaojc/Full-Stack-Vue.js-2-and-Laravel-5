56fd7d8a4f2bcd5b3023dd163113cc499c9befb0
serenaliaojc <36046972+serenaliaojc@users.noreply.github.com>
2019-06-27 12:56:32 +0800

    prototype: style binding

--- vuebnb-prototype/app.js
+++ vuebnb-prototype/app.js
@@ -1,8 +1,11 @@
 var app = new Vue({
     el: '#app',
     data: {
-        title: 'My Appointment',
-        address: '12 My Street, My City, My Country',
-        about: 'This is a description of my apartment.'
+        title: sample.title,
+        address: sample.address,
+        about: sample.about,
+        headerImageStyle: {
+            'background-image': 'url(sample/header.jpg)'
+        }
     }
 });
